53180e7e2c1d9884393cd869ff6c0761fa7cf8b7
Scott Pellett <spellett@gmail.com>
2013-05-03 21:17:57 -0700

    Added redirect on index page to take you to the term search since we have nothing on the index page. Modified the user page to display the user's twitter profile pic but we still need to add a api call to pull image from twitter if no tweets found. Updated term page to pull the query from the post params and uses stockr as the default query. Still need to hook up the javascript to perform the async post.

--- stacksocial/views.py
+++ stacksocial/views.py
@@ -1,18 +1,20 @@
 import plugins.twitter as twitter
 
 from django.http import HttpResponse
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, redirect
 from django.template import RequestContext
 
 def index(request):
-    template_vars = {}
-    return render_to_response('index.html', template_vars, context_instance=RequestContext(request))
+    return redirect('term_page')
+    #template_vars = {}
+    #return render_to_response('index.html', template_vars, context_instance=RequestContext(request))
 
 def term_tweets(request):
-    template_vars = {}
+    query = request.REQUEST.get('query', 'stockr')
 
-    search_results = twitter.get_tweets_by_term('venice')
+    search_results = twitter.get_tweets_by_term(query)
   
+    template_vars = {}
     template_vars['tweets'] = search_results.get('statuses')
 
     return render_to_response('termTweets.html', template_vars, context_instance=RequestContext(request))
@@ -22,6 +24,10 @@ def user_tweets(request, handle, **kwargs):
 
     template_vars['handle'] = handle
 
-    template_vars['tweets'] = twitter.get_tweets_by_user(handle)
+    tweets = twitter.get_tweets_by_user(handle)
+    template_vars['tweets'] = tweets
+
+    if tweets:
+        template_vars['profile_img_url'] = tweets[0].get('user').get('profile_image_url')
 
     return render_to_response('userTweets.html', template_vars, context_instance=RequestContext(request))
